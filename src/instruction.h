280de62ad0b5daea3ee8a1295e0b65f320583c72
Nautigsam <aurelien.bertron@insa-lyon.fr>
2015-03-10 10:39:07 +0100

    ajoute membres instruction

--- src/instruction.h
+++ src/instruction.h
@@ -4,29 +4,37 @@
 #define SRC_INSTRUCTION_H_
 
 #include "./token.h"
+#include "./expr.h"
 
 class Instruction : public Token {
 public:
-  //Instruction() {}
+  explicit Instruction(Token::Id id) : Token(id) {}
   virtual ~Instruction() {}
 };
 
 class Read : public Instruction {
 public:
-  //Read() {}
+  Read(Token::Id id, Variable *_pV) : Instruction(id), pV(_pV) {}
   virtual ~Read() {}
+protected:
+  Variable *pV;
 };
 
 class Write : public Instruction {
 public:
-  //Write() {}
+  Write(Token::Id id, Variable *_pV) : Instruction(id), pV(_pV) {}
   virtual ~Write() {}
+protected:
+  Variable *pV;
 };
 
 class Assignment : public Instruction {
 public:
-  //Assignment() {}
+  Assignment(Token::Id id, Variable *_pV, Expr *_pE) : Instruction(id), left(_pV), right(_pE) {}
   virtual ~Assignment() {}
+protected:
+  Variable *left;
+  Expr *right;
 };
 
 #endif //SRC_INSTRUCTION_H_
