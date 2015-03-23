54549c7143dc9ed86ee0b29b91774a177f19a53d
Gustave (Gus) Arthur Monod <gustave.monod@insa-lyon.fr>
2015-03-23 17:43:27 +0100

    Forgot one file

--- src/errors.h
+++ src/errors.h
@@ -13,10 +13,13 @@ inline void myassert(bool assertion, const char * const what = "") { }
 
 #else
 
-inline void MYASSERT(bool assertion, char const * file,
+inline void MYASSERT(bool assertion, const char * const file,
                                      int line,
                                      const char * const what = "") {
-    if(!assertion) std::cerr << what << " in " << file << ":" << line << std::endl;
+    if (!assertion) {
+        std::cerr << what << " in " << file << ":" << line << std::endl;
+    }
+
     assert(assertion);
 }
 #define myassert(assertion, what) MYASSERT(assertion, __FILE__, __LINE__, what)
@@ -27,6 +30,12 @@ class compile_error : public std::runtime_error {
  public:
     explicit compile_error(const char *what) : std::runtime_error(what) {}
     explicit compile_error(const std::string &what): std::runtime_error(what){}
+};
+
+class lexical_error : public std::runtime_error {
+ public:
+    explicit lexical_error(const char *what) : std::runtime_error(what) {}
+    explicit lexical_error(const std::string &what): std::runtime_error(what){}
 };
 
 #endif  // SRC_ERRORS_H_
